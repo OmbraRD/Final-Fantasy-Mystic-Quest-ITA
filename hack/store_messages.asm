0272915b062f94b5751af46ee6ed5a73e54bf3c1
OmbraRD <64612056+OmbraRD@users.noreply.github.com>
2021-01-04 17:41:02 -0400

    Store messages, remove "s" and "et"

--- hack/store_messages.asm
+++ hack/store_messages.asm
@@ -2,10 +2,18 @@ arch 65816
 lorom
 
 ;;
-;; REMOVE S AT THE END OF OBJECTS
+;; REMOVE "s" AND "et" AT THE END OF OBJECTS
 ;;
-org $3FE88
+org $3FE7D
+    db $0F,$00,$15
+    db $08,$83,$83
+    db $05,$0B,$FC
+    db $89,$FE
     ; db $C6 ; s
-    db $05,$0B,$FA,$90,$FE
-    db $FF
-
+    db $05,$0B,$FA
+    db $90,$FE
+    db $05,$0B,$FA
+    db $90,$FE
+    ; db $B8,$C7 ; et
+    db $00,$00 ; FILL
+    db $00
