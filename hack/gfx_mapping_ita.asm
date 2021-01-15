b509074ad3fa250e1b382c2661455ee06b3ae19f
OmbraRD <64612056+OmbraRD@users.noreply.github.com>
2021-01-14 23:17:14 -0400

    Preliminary work on "MAGIC LEFT" in battle/spell menu. Added translation for MANUAL in gfx

--- hack/gfx_mapping_ita.asm
+++ hack/gfx_mapping_ita.asm
@@ -1,4 +1,5 @@
-norom
+arch 65816
+lorom
 
 ; NOTES:
 ; STATUS MENU LAYOUT: 0x393F0 to 0x3947A SNES
@@ -8,7 +9,7 @@ norom
 ;;
 
 ; WINDOW
-org $19449
+org $039449
     db $24 ; WINDOW OPCODE
     db $0B ; X POSITION
     db $11 ; Y POSITION
@@ -17,11 +18,11 @@ org $19449
     db $15,$0D ; CONTENT POSITION
     
 ; POINTER
-org $19452
+org $039452
     db $08,$26,$9D
 
 ; TILES
-org $19D26 ; 39D26 SNES
+org $039D26
     db $FF,$E8,$E9,$EA,$EB ; DIFESA
     db $02
     db $F0,$F1,$F2,$F3,$F4,$F5 ; ELEMENTALE
@@ -34,7 +35,7 @@ org $19D26 ; 39D26 SNES
 ;;
 ;; MENU - WEAPONS
 ;;
-org $19977
+org $039977
     ; WEAPONS LEFT ... ... ... ...
     db $DE,$DF,$FF,$E6,$E7,$FF,$D8,$D8,$D8,$D8,$D8,$FF
     
@@ -69,15 +70,15 @@ org $19977
 ;; 2880 d8d9dadb 2800
 ;; 00
 
-org $198BC ; MAGIC
+org $0398BC ; MAGIC
     db $DA,$DB,$DC,$DD
-org $198E3 ; LEFT
+org $0398E3 ; LEFT
     db $DE,$DF
-org $198EF ; WHITE
+org $0398EF ; WHITE
     db $D0,$D1,$D2,$D3
-org $198F7 ; BLACK
+org $0398F7 ; BLACK
     db $D4,$D5,$D6,$D7
-org $198FF ; WIZARD
+org $0398FF ; WIZARD
     db $FF,$D8,$D9,$FF
 
 ;; TEST INVERSIONE QTA. MAGIA
@@ -100,14 +101,14 @@ org $198FF ; WIZARD
 ;;
 ;; MENU - CUSTOMIZE - FAST/SLOW
 ;;
-org $19DA9
+org $039DA9
     db $E0,$E1 ; FAST
     db $08,$F7,$FF ; JUMP TO FFFFFFFFFFFFFFFFFF00
 
-org $19DAE
+org $039DAE
     db $E4,$E5 ; SLOW
 
-org $1FFF7
+org $03FFF7
     db $E2,$FF,$FF,$FF,$FF,$FF,$FF,$FF,$E3
     ; This should be 00 terminated but by sheer luck, once
     ; the ROM is in RAM the byte at offset 0x40000 is 00
@@ -116,7 +117,7 @@ org $1FFF7
 ;; NEW GAME SCREEN - END
 ;; Change last byte
 ;;c
-org $1ACF9 ; 3ACF9 SNES
+org $03ACF9
     db $05,$75,$47 ; E
     db $05,$75,$48 ; N
     db $05,$75,$49 ; D
@@ -124,12 +125,12 @@ org $1ACF9 ; 3ACF9 SNES
 ;;
 ;; BATTLE - AUTO/MANUAL
 ;;
-org $1B721
+org $03B721
     ; db $28,$80
     db $BA,$BB ; AUTO
     ; db $28,$00
 
-org $1B72A
+org $03B72A
     ; db $28,$80
     db $BC,$BD,$BE ; MANUAL
     ; db $28,$00
@@ -137,7 +138,57 @@ org $1B72A
 ;;
 ;; ALL MENUS - LEVEL
 ;;
-org $1B7B2 ; 3B7B2 SNES
+org $03B7B2 ; 3B7B2 SNES
     ; db $28,$80
     db $CB,$CC,$CD,$CE,$CF
     ; db $28,$00
+
+
+;;
+;; BATTLE - SPELL SUBMENU - BLACK/WHITE/WIZARD MAGIC LEFT
+;;
+
+;org $3A2E1
+;    db $05,$dc,$03 ; OPCODE 2 BYTES, AMOUNT OF ARGS
+;    db $a31a ; POINTER TO WHITE
+;    db $a328 ; POINTER TO BLACK
+;    db $a30c ; POINTER TO WIZARD
+;
+;org $3A2EA
+;    ;
+;    db $28,$80
+;    db $dc,$dd,$de,$df ; MAGIC
+;    db $28,$00
+;    db $de,$df,$ff ; LEFT
+;    db $05,$22
+;    db $08,$df,$81 ; POINTER?
+;    db $08,$ec,$a1 ; POINTER?
+;    db $05,$8d,$0f
+;    db $02
+;    db $00
+;    db $05,$4d,$18,$13,$70
+;    db $09,$9f,$d2,$00
+;    db $00
+;
+;org $3A30C
+;    db $05,$f4,$5f,$01,$1a0
+;    db $28,$80
+;    db $d8,$d9,$da,$db ; WIZARD
+;    db $28,$00
+;    db $00
+;
+;org $3A31A
+;    db $05,$f4,$5f,$01,$18
+;    db $28,$80
+;    db $d0,$d1,$d2,$d3 ; WHITE
+;    db $28,$00
+;    db $00
+;
+;org $3A328
+;    db $05,$f4,$5f,$01,$19
+;    db $28,$80
+;    db $d4,$d5,$d6,$d7 ; BLACK
+;    db $28,$00
+;    db $00
+;
+;warnpc $3A336
